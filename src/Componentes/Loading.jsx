6c148e4f400afc40493fa756456930baa3ca9989
FedeCalvino <calvinofede@gmail.com>
2024-06-24 19:17:16 -0300

    spinner

---
+++ src/Componentes/Loading.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ClipLoader } from "react-spinners";
+
+
+export const Loading = ({tipo}) => {
+    console.log(tipo)
+        if(tipo=="tab"){
+        return (
+            <>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
+                <ClipLoader color={"#123abc"} loading={true} size={80} />
+            </div>
+            </>
+        );
+        } 
+        if(tipo=="all"){
+            return (
+                <>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                <ClipLoader color={"#123abc"} loading={true} size={100} />
+                </div>
+                </>
+            );
+        }   
+    
+}
