7bfa3a695b40972560198df43ce86595a8358580
FedeCalvino <calvinofede@gmail.com>
2024-06-25 14:02:19 -0300

    nav bar

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -14,30 +14,31 @@ export const NavBar = ({ logged, onLogout }) => {
 
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container className='container_nav'>
-          <Navbar.Brand href="#home" className="brand-custom">Anne Decor</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="Clientes">Clientes</Nav.Link>
-              <Nav.Link href="Ventas">Ventas</Nav.Link>
-              <Nav.Link href="CrearVenta">Crear Venta</Nav.Link>
-              <Nav.Link href="Facturas">Facturas</Nav.Link>
-              <Nav.Link href="WareHouse">WareHouse</Nav.Link>
-              <Nav.Link href="Instalaciones">Instalaciones</Nav.Link>
-              <NavDropdown title="Seguimiento Estaciones" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Tela</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Caño</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Armado</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            {logged ? (
-              <Button variant='danger' onClick={()=>handleLogout()}>LogOut</Button>
-            ) :null}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+       <Navbar expand="lg" className="bg-body-tertiary">
+            <Container className='container_nav'>
+                <Navbar.Brand href="#home" className="brand-custom">Anne Decor</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="Provedores" className="nav-link-custom">Provedores</Nav.Link>
+                        <Nav.Link href="Clientes" className="nav-link-custom">Clientes</Nav.Link>
+                        <Nav.Link href="Ventas" className="nav-link-custom">Ventas</Nav.Link>
+                        <Nav.Link href="CrearVenta" className="nav-link-custom">Crear Venta</Nav.Link>
+                        <Nav.Link href="Facturas" className="nav-link-custom">Facturas</Nav.Link>
+                        <Nav.Link href="WareHouse" className="nav-link-custom">WareHouse</Nav.Link>
+                        <Nav.Link href="Instalaciones" className="nav-link-custom">Instalaciones</Nav.Link>
+                        <NavDropdown title="Seguimiento Estaciones" id="basic-nav-dropdown" className="nav-link-custom">
+                            <NavDropdown.Item href="#action/3.1">Tela</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Caño</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Armado</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    {logged ? (
+                        <Button variant='danger' onClick={handleLogout}>LogOut</Button>
+                    ) : null}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </>
   );
 };
