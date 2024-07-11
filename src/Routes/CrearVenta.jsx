3185dad2e631e610d80ed79005f0db8124f60420
FedeCalvino <Calvinofede@gmail.com>
2024-07-11 14:10:23 -0300

    amb

--- src/Routes/CrearVenta.jsx
+++ src/Routes/CrearVenta.jsx
@@ -184,7 +184,7 @@ export const CrearVenta = () => {
                     <Form.Group as={Col} md="1" style={{ width: "11%" }} noValidate>
                         <Form.Label style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Ambiente</Form.Label>
                         <Form.Select as={Col} md="1" aria-label="Default select example" onChange={handleSelectAmbienteChange} value={selectedAreaRoler}>
-                            <option style={{ textAlign: "center" }}>Ambiente</option>
+                            <option style={{ textAlign: "center" }}></option>
                             {Ambientes.map(amb =>
                                 <option style={{ textAlign: "center" }} value={amb.Nombre} key={amb.Nombre}>
                                     {amb.Nombre}
